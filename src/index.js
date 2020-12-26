71e79f0fa0f4a3c366fe40ba2d62fb55f8c93c28
nnafzaoui <nafzaouinourdine@gmail.com>
2020-12-26 19:52:35 +0100

    dev tools

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 Menu.setApplicationMenu(false);
 // This method will be called when Electron has finished
